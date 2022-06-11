6e5362a3b48f85bf0415158d6a8c6b0e6c491ed9
Jesús Velásquez <rgb2black@gmail.com>
2022-06-11 02:48:19 -0400

    Version beta del proyecto web desarrollado con PHP

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Tales Chamber - Base de Datos</title>
+		<meta charset="utf-8">
+		<!-- <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.css"> -->
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+
+		<section id="menu-principal">
+			<button id="boton-menu-iniciar-sesion" class="boton-menu">Iniciar sesión</button>
+			<button id="boton-menu-registro" class="boton-menu">Registro</button>
+			<button id="boton-menu-acercade" class="boton-menu">Acerca de </button>
+		</section>
+
+		<section id="interfaz-inicio-de-sesion">
+
+			<img src="img/S.png" id="logo">
+
+			<div class="formulario">
+				<input type="text" name="usuario" required maxlength="10" minlength="5" autocomplete="off" placeholder="Usuario">
+
+				<input type="password" name="clave" required maxlength="10" minlength="5" autocomplete="off" placeholder="Contraseña">
+
+				<button id="iniciar-sesion" class="boton-inicio-de-sesion">Entrar</button>
+			</div>
+
+			<button id="boton-inicio-de-sesion-volver" class="boton-inicio-de-sesion">Volver</button>
+		</section>
+
+		<section id="interfaz-registro">
+
+			<h3 class="reg-label">Por favor, ingrese sus datos</h3>
+
+			<div class="formulario">
+				<input type="text" name="usuario" required maxlength="10" minlength="5" autocomplete="off" placeholder="Usuario">
+
+				<input type="password" name="clave" required maxlength="10" minlength="5" autocomplete="off" placeholder="Contraseña">
+
+				<input type="text" name="usuario" required maxlength="10" minlength="5" autocomplete="off" placeholder="Confirme su usuario">
+
+				<input type="password" name="clave" required maxlength="10" minlength="5" autocomplete="off" placeholder="Confirme su contraseña">
+
+				<button id="registrarse" class="breg">Registrarse</button>
+			</div>
+
+			<button id="breg-volver" class="breg">Volver</button>
+		</section>
+
+		<main id="panel-de-usuario"></main>
+	</body>
+
+	<script src="script.js"></script>
+</html>
---
+++ script.js
@@ -0,0 +1,106 @@
+"use strict";
+
+function Eventos_del_Menu_Principal(){
+
+	let bm_iniciar_sesion = document.querySelector("#boton-menu-iniciar-sesion"),
+		bm_registro = document.querySelector("#boton-menu-registro"),
+		botonISVolver = document.querySelector("#boton-inicio-de-sesion-volver"),
+		botonRegVolver = document.querySelector("#breg-volver"),
+		botonAcercaDe = document.querySelector("#boton-menu-acercade");
+
+
+	bm_iniciar_sesion.addEventListener("click",()=>{
+		document.querySelector("#menu-principal").style.display="none";
+		document.querySelector("#interfaz-inicio-de-sesion").style.display="flex";
+	});
+
+	bm_registro.addEventListener("click",()=>{
+		document.querySelector("#menu-principal").style.display="none";
+		document.querySelector("#interfaz-registro").style.display="flex";
+	});
+
+	botonISVolver.addEventListener("click",()=>{
+		document.querySelector("#menu-principal").style.display="flex";
+		document.querySelector("#interfaz-inicio-de-sesion").style.display="none";
+	});
+
+	botonRegVolver.addEventListener("click",()=>{
+		document.querySelector("#menu-principal").style.display="flex";
+		document.querySelector("#interfaz-registro").style.display="none";
+	});
+
+	botonAcercaDe.addEventListener("click",()=>{
+
+		let ventanaModal = document.createElement("DIV"),
+			articuloVentanaModal = document.createElement("ARTICLE"),
+			primerParrafoVentanaModal = document.createElement("P"),
+			segundoParrafoVentanaModal = document.createElement("P"),
+			botonVentanaModal = document.createElement("BUTTON");
+
+		ventanaModal.setAttribute("class","ventana-modal-acercade");
+		ventanaModal.setAttribute("style",`
+					position:fixed;
+					top:0;
+					left:0;
+					width:100%;
+					height:100%;
+					display:flex;
+					flex-flow:row nowrap;
+					justify-content:center;
+					align-items:center;
+					background-color:rgba(20,20,20,0.8);
+					color:white;
+		`);
+
+		articuloVentanaModal.setAttribute("style","text-align:center;");
+
+		primerParrafoVentanaModal.setAttribute("class","parrafo-ventana-modal-acercade");
+		primerParrafoVentanaModal.textContent="Desarrollado por: Jesús Velásquez.";
+
+		segundoParrafoVentanaModal.setAttribute("class","parrafo-ventana-modal-acercade");
+		segundoParrafoVentanaModal.textContent="Versión: Beta";
+
+		botonVentanaModal.setAttribute("style",`
+							color:white;
+							background-color: #4A8D4A;
+							border:none;
+							border-radius:5px;
+							padding:2%;
+							margin-top:5%;
+							cursor:pointer;
+		`);
+
+		botonVentanaModal.setAttribute("class","cerrar-ventana-modal-acercade");
+		botonVentanaModal.textContent="Cerrar";
+
+		articuloVentanaModal.appendChild(primerParrafoVentanaModal);
+		articuloVentanaModal.appendChild(segundoParrafoVentanaModal);
+		articuloVentanaModal.appendChild(botonVentanaModal);
+
+		ventanaModal.appendChild(articuloVentanaModal);
+
+		document.querySelector("body").insertAdjacentElement("beforeEnd",ventanaModal);
+
+		document.querySelector(".cerrar-ventana-modal-acercade").addEventListener("click",()=>{
+			document.querySelector("body").removeChild(document.querySelector(".ventana-modal-acercade"));
+		});
+	});
+}
+
+function Eventos_de_Registro(){
+	// Code
+}
+
+function Eventos_de_Inicio_de_Sesion(){
+	// Code
+}
+
+
+window.addEventListener("load",()=>{
+
+	Eventos_del_Menu_Principal();
+	Eventos_de_Registro();
+	Eventos_de_Inicio_de_Sesion();
+
+});
+
